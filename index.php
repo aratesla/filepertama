6df0141d7b8c3d0938ed701b0cfad87b8c438937
aratesla <79205278+aratesla@users.noreply.github.com>
2021-02-17 20:29:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+#SCRIPT CREATED BY MR.KHAY_ADW
+#AUTHOR : MR.KHAY ADW
+Thanks to :#gaibcybert | NETCARD CR3W 
+ 
+<html>
+<title>HACKED BY NETCARD CR3W</title>
+<head>
+<tt>
+<style>
+      h1 {
+    color: black;
+    font-size: 40px;
+    margin: 1px auto;
+    text-align:center;
+    text-transform:uppercase;
+        }
+    .neon {
+    color: green;
+    text-shadow: 0 0 5px #19abff, 0 0 10px #19abff, 0 0 20px #19abff, 0 0 45px #19abff, 0 0 40px #19abff;
+    }
+    </style>
+ 
+<script language="JavaScript1.2">
+function ClearError() {return true;}
+window.onerror = ClearError;
+</script>
+<script>
+window.onload = function() {
+var h1 = document.getElementsByTagName("h1")[0],
+text = h1.innerText || h1.textContent,
+split = [], i, lit = 0, timer = null;
+for(i = 0; i < text.length; ++i) {
+split.push("<span>" + text[i] + "</span>");
+}
+h1.innerHTML = split.join("");
+split = h1.childNodes;
+ 
+var flicker = function() {
+lit += 0.01;
+if(lit >= 1) {
+clearInterval(timer);
+}
+for(i = 0; i < split.length; ++i) {
+if(Math.random() < lit) {
+split[i].className = "neon";
+} else {
+split[i].className = "";
+}
+}
+}
+setInterval(flicker, 300);
+}
+</script>
+<br><br>
+<h1>HACKED by NETCARD CR3W</h1><body bgcolor="black">
+<font color="maroon">
+<p>
+</p>
+<center><h3>OOopss... SORRY SIR YOUR SITE HAS BEEN HACKED</h3></center>
+ 
+<center><img src="https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2014/01/googles-new-portal-provides-help-for-hacked-sites.jpg"width="450"height="450"></center>
+<p>
+</p>
+<h2>
+<script language="JavaScript1.2">
+ 
+/*
+Neon Lights Text
+By JavaScript Kit (http://javascriptkit.com)
+Over 400+ free scripts here!
+*/
+ 
+var message="Accses_root#: Exploit_succses$ enter_ip_adress>ab.0p.0xO.101$>enter_messages$:site has been hacked by mr.khay_adw patch your system admin"
+var neonbasecolor="black"
+var neontextcolor="green"
+var flashspeed="300" //in milliseconds
+ 
+///No need to edit below this line/////
+ 
+var n=0
+if (document.all||document.getElementById){
+document.write('')
+for (m=0;m<message.length;m++)
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+document.write('')
+}
+else
+document.write(message)
+ 
+function crossref(number){
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
+return crossobj
+}
+ 
+function neon(){
+ 
+//Change all letters to base color
+if (n==0){
+for (m=0;m<message.length;m++)
+//eval("document.all.neonlight"+m).style.color=neonbasecolor
+crossref(m).style.color=neonbasecolor
+}
+ 
+//cycle through and change individual letters to neon color
+crossref(n).style.color=neontextcolor
+ 
+if (n<message.length-1)
+n++
+else{
+n=0
+clearInterval(flashing)
+setTimeout("beginneon()",1500)
+return
+}
+}
+ 
+function beginneon(){
+if (document.all||document.getElementById)
+flashing=setInterval("neon()",flashspeed)
+}
+beginneon()
+ 
+ 
+</script>
+</h2>
+<center><font size="25px">|#ZOYONE | #UCHUPZ | #IBNU | #AND YOU |</font></center></tt>
+</html>
